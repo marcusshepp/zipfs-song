f9d219f4ff95b399c8c1fdf25c69d397ee9e09c5
sheph2mj <sheph2mj@cmich.edu>
2016-04-15 13:05:37 -0400

    test function

--- solution.py
+++ solution.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python
 """
 A programming challenge proposed by Savant Group.
 (http://www.savantgroup.com/)
@@ -8,21 +9,45 @@ Original Problem Source: https://labs.spotify.com/puzzles/
 Solution By: 
 Marcus J. Shepherd (marcusshepdotcom@gmail.com)
 """
+import sys
 from collections import namedtuple
 
+def test_algo():
+    def test_w_file(file_path, desired_result):
+        with open(file_path) as f:
+            a_s, s_s = map(int, f.readline().split(" "))
+            album = list()
+            for _ in xrange(a_s):
+                plays, name = plays_name_from_file(f)
+                album.append((int(plays), strip_newline(name)))
+            assert algo(to_namedtuple(album), a_s, s_s) == desired_result
+    test_w_file(
+        file_path="test_input/input1.txt", 
+        desired_result=["four", "two"])
+    test_w_file(
+        file_path="test_input/input2.txt", 
+        desired_result=["19_2000", "clint_eastwood", "tomorrow_comes_today"])
+    
 def albumsize_selectsize():
-    albumsize, selectsize = raw_input().split(" ")
+    albumsize, selectsize = map(int, sys.stdin.readline().split(" "))
     return albumsize, selectsize
 
 def plays_name():
-    plays, name = raw_input().split(" ")
+    plays, name = sys.stdin.readline().split(" ")
     return int(plays), name
+    
+def plays_name_from_file(f):
+    plays, name = f.readline().split(" ")
+    return strip_newline(plays), strip_newline(name)
+
+def strip_newline(s):
+    return s.replace("\n", "")
     
 def to_namedtuple(tracks):
     temp_tracks = list()
     for plays, name in tracks:
         track = namedtuple("Track", ["name", "plays", "quality"])
-        track.name = name
+        track.name = strip_newline(name)
         track.plays = plays
         temp_tracks.append(track)
     return temp_tracks
@@ -44,11 +69,15 @@ def algo(tracks, albumsize, selectsize):
                 reverse=True)[:selectsize]
     return [track.name for track in new_tracks]
 
-def main():
-    albumsize, selectsize = albumsize_selectsize()
-    tracks = to_namedtuple([plays_name() for _ in xrange(int(albumsize))])
-    for i in algo(tracks, int(albumsize), int(selectsize)):
-        print i
+def main(DEBUG):
+    if DEBUG:
+        return test_algo()
+    else:
+        albumsize, selectsize = albumsize_selectsize()
+        tracks = to_namedtuple([plays_name() for _ in xrange(albumsize)])
+        for i in algo(tracks, albumsize, selectsize):
+            print i
 
 if __name__ == '__main__':
-    main()
+    if sys.stdin.isatty():
+        main(DEBUG=False)
