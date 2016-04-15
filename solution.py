78af3370450aa77bcaad8201e68497b0db3a1c2d
sheph2mj <sheph2mj@cmich.edu>
2016-04-15 14:09:05 -0400

    possible solution for dublicate track quality in results

--- solution.py
+++ solution.py
@@ -10,17 +10,17 @@ Solution By:
 Marcus J. Shepherd (marcusshepdotcom@gmail.com)
 """
 import sys
-from collections import namedtuple
+import collections
+import itertools
 
 def test_algo():
     def test_w_file(file_path, desired_result):
         with open(file_path) as f:
             a_s, s_s = map(int, f.readline().split(" "))
-            album = list()
-            for _ in xrange(a_s):
-                plays, name = plays_name(f)
-                album.append((int(plays), strip_newline(name)))
-            assert algo(to_namedtuple(album), a_s, s_s) == desired_result
+            original_track_tuples = (
+                plays_name(f) for _ in xrange(a_s))
+            tracks = to_namedtuple(original_track_tuples)
+            assert algo(tracks, original_track_tuples, a_s, s_s) == desired_result
     test_w_file(
         file_path="test_input/input1.txt", 
         desired_result=["four", "two"])
@@ -30,25 +30,35 @@ def test_algo():
     
 def albumsize_selectsize():
     albumsize, selectsize = map(int, sys.stdin.readline().split(" "))
-    return albumsize, selectsize
+    algo_info = (albumsize, selectsize)
+    return algo_info
 
 def plays_name(input_):
     plays, name = input_.readline().split(" ")
-    return int(plays), name
-
+    track = (int(plays), name)
+    return track
+    
 def strip_newline(s):
     return s.replace("\n", "")
+    
+def align(l1, l2):
+    """ 
+    Used to align the resulting list with the original list.
+    Called only when in need of a track quality tie breaker.
+    """
+    new_l2 = [y for x in l1 for y in l2 if x == y]
+    return new_l2
     
 def to_namedtuple(tracks):
     temp_tracks = list()
     for plays, name in tracks:
-        track = namedtuple("Track", ["name", "plays", "quality"])
+        track = collections.namedtuple("Track", ["name", "plays", "quality"])
         track.name = strip_newline(name)
         track.plays = plays
         temp_tracks.append(track)
     return temp_tracks
     
-def algo(tracks, albumsize, selectsize):
+def algo(tracks, original_track_tuples, albumsize, selectsize):
     """
     in: tracks = (plays, name) with len = albumsize
     """
@@ -64,17 +74,31 @@ def algo(tracks, albumsize, selectsize):
                 key=lambda track: track.quality, 
                 reverse=True)[:selectsize]
     assert len(new_tracks) == selectsize
-    return [track.name for track in new_tracks]
+    duplicates = list()
+    for track1, track2 in itertools.combinations(new_tracks, 2):
+        if track1.quality == track2.quality:
+            duplicates.append(track1)
+            duplicates.append(track2)
+    if duplicates:
+        aligned = align(
+            [t.name for t in duplicates], 
+            [tup[0] for tup in original_track_tuples])
+        assert len(aligned) == selectsize
+        return aligned
+    return "\n".join([track.name for track in new_tracks])
 
 def main(DEBUG):
     if DEBUG:
         test_algo()
     else:
         albumsize, selectsize = albumsize_selectsize()
-        tracks = to_namedtuple([plays_name(sys.stdin) for _ in xrange(albumsize)])
-        for i in algo(tracks, albumsize, selectsize):
-            sys.stdout.write(i)
+        original_track_tuples = (
+            plays_name(sys.stdin) for _ in xrange(albumsize))
+        tracks = to_namedtuple(original_track_tuples)
+        results = algo(tracks, original_track_tuples, albumsize, selectsize)
+        for name in results:
+            sys.stdout.writelines(name)
 
 if __name__ == '__main__':
     if sys.stdin.isatty():
-        main(DEBUG=True)
+        main(DEBUG=False)
